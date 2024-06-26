462850668b479ccd5ecb2e7b0a41aea00813b95c
Hirokazu Okawa <h.okyawa@gmail.com>
2024-06-26 15:28:55 +0900

    add: TailwindCSSの導入

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: 'jit',
+  content: ['./src/**/*.{html,js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
