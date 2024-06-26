a3564cfb4272a9d50df781accd79d25a89f6d414
Hirokazu Okawa <h.okyawa@gmail.com>
2024-06-26 15:20:08 +0900

    add: vite.configにbuildの設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src',
   plugins: [react()],
+  build: {
+    // distフォルダに出力
+    outDir: resolve(__dirname, 'dist'),
+    // 存在しないときはフォルダを作成する
+    emptyOutDir: true,
+    rollupOptions: {
+      // entry pointがあるindex.htmlのパス
+      input: {
+        '': resolve(__dirname, 'src/index.html'),
+      },
+      // bundle.jsを差し替えする
+      output: {
+        entryFileNames: `assets/[name]/bundle.js`,
+      },
+    },
+  },
 })
