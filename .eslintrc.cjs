a0880bd89949e47ab199655876515d096dac137a
Hirokazu Okawa <h.okyawa@gmail.com>
2024-06-26 15:38:16 +0900

    add: ESLintの導入
    npm i -D eslint typescript @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint-plugin-react eslint-plugin-react-hooks
    
    npx eslint --init
    
    npm install --save-dev eslint-config-prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,110 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/typescript',
+    'plugin:import/recommended',
+    'plugin:import/warnings',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: '*.stories.@(ts|tsx|js)',
+      extends: 'plugin:storybook/recommended',
+      rules: {
+        'import/no-default-export': 'off', // default export を許可
+      },
+    },
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+      pragma: 'h',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      typescript: { project: './' },
+    },
+  },
+  env: {
+    es2022: true,
+  },
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   rules: {
-    'react-refresh/only-export-components': [
+    'space-before-function-paren': [
       'warn',
-      { allowConstantExport: true },
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always',
+      },
     ],
+    'no-useless-constructor': 'off',
+    'no-use-before-define': 'off',
+    'comma-dangle': ['warn', 'always-multiline'],
+    // 'no-unused-vars': 'warn',
+    semi: ['warn', 'always'],
+    'semi-spacing': [
+      'warn',
+      {
+        after: true,
+        before: false,
+      },
+    ],
+    'semi-style': ['warn', 'last'],
+    'no-extra-semi': 'warn',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'warn',
+    'padded-blocks': ['warn', 'never'],
+    'no-trailing-spaces': 'warn',
+    'space-before-blocks': 'warn',
+    'no-multiple-empty-lines': 'warn',
+    indent: [
+      'warn',
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    'eol-last': 'warn',
+    quotes: [
+      'warn',
+      'single',
+      {
+        allowTemplateLiterals: true,
+      },
+    ],
+    'import/order': [
+      'warn',
+      {
+        groups: ['external', 'parent', 'sibling', 'index', 'object', 'type'],
+        pathGroups: [
+          {
+            pattern: '@alias/**',
+            group: 'parent',
+            position: 'before',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+        },
+        'newlines-between': 'always',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
   },
 }
