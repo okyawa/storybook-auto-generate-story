581708761179ffd0229c8d2b71b7bbdb1e1d0f3c
Hirokazu Okawa <h.okyawa@gmail.com>
2024-06-26 15:40:00 +0900

    add: Prettierの導入

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'all',
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'off',
+};
